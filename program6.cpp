a39ae6a5f899fa766fe6574b4d6ce2ecc51b9aa7
ved malpe <vedmalpe21@gmail.com>
2025-10-07 20:40:47 +0530

    Create program6.cpp

---
+++ program6.cpp
@@ -0,0 +1,18 @@
+//Concatenation of two strings
+#include<iostream>
+using namespace std;
+int main()
+{
+    string a,b;
+    cout<<"Enter the string a: ";
+    cin>>a;
+    cout<<"Enter the string b: ";
+    cin>>b;
+    cout<<"Concatenation of the strings: "<<a+b;
+    return 0;
+}
+/*output:
+Enter the string a: Tarushi
+Enter the string b: Verma
+Concatenation of the strings: TarushiVerma
+*/
